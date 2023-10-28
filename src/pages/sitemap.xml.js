b86512d30b897da0f0329d036cbe0a143bd1db50
kenny <r1dpanduh@gmail.com>
2023-10-29 03:30:02 +0800

    Create sitemap.xml.js

---
+++ src/pages/sitemap.xml.js
@@ -0,0 +1,11 @@
+// pages/sitemap.xml.js
+
+const Sitemap = () => {
+  return null
+}
+
+export const getServerSideProps = async ({ res }) => {
+  // You can create a sitemap here.
+}
+
+export default Sitemap
