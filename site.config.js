b78d807d7c4fff104ac6c0ff0e74f85fab289cde
kenny <r1dpanduh@gmail.com>
2023-10-29 14:16:52 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Kenny",
-    image: "/avator.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Software Development Engineer",
     bio: "I develop everything using Java.",
     email: "",
